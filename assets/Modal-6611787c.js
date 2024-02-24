a37b40e5256a3ec0189ddc0d7270d5cd68c81d9f
ahmedhaanyy <a.hany@complade.com>
2024-02-24 15:57:47 +0200

    Updates

--- assets/Modal-6611787c.js
+++
@@ -1,1 +0,0 @@
-import{j as e,M as l}from"./index-429ef916.js";function t({children:o,open:s,setOpen:a}){return e.jsx(l,{open:s,onClose:()=>a(!1),disableScrollLock:!0,children:o})}export{t as default};
